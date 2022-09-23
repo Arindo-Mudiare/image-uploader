385bcd1cb37ec7daa81fb706e545af98408a252b
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-09-23 18:35:30 +0100

    setup cloudinary upload

---
+++ controllers/uploadsController.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const uploadProductImage = async (req, res) => {
+  //   check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError(`No file uploaded`);
+  }
+
+  const productImage = req.files.image;
+
+  // check file upload format
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError(`Please upload an Image File`);
+  }
+  const maxSize = 1024 * 1024;
+  // check file image size limit
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      `Please upload Image not larger than 1kb`
+    );
+  }
+  const imagePath = path.join(
+    __dirname,
+    `../public/uploads/${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = { uploadProductImage };
